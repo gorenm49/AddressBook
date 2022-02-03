039bcab6c91347683b6c672b2045c83c57d8081d
Nitya <gorenityanand96@gmail.com>
2022-02-03 17:12:40 +0530

    [Nityanand]:Contact add to addressBook Added

--- Program.cs
+++ Program.cs
@@ -8,30 +8,8 @@ namespace AddressBook
         {
             System.Console.WriteLine("Welcome to Address Book Program!!!");
 
-            Contacts contacts = new Contacts();
-            System.Console.WriteLine("Enter First Name: ");
-            contacts.FirstName = Console.ReadLine();
-
-            System.Console.WriteLine("Enter Last Name: ");
-            contacts.LastName = Console.ReadLine();
-
-            System.Console.WriteLine("Enter Adress: ");
-            contacts.Address = Console.ReadLine();
-
-            System.Console.WriteLine("Enter City: ");
-            contacts.City = Console.ReadLine();
-
-            System.Console.WriteLine("Enter State: ");
-            contacts.State = Console.ReadLine();
-
-            System.Console.WriteLine("Enter Zip: ");
-            contacts.Zip = Convert.ToInt32(Console.ReadLine());
-
-            System.Console.WriteLine("Phone Number: ");
-            contacts.PhoneNumber = Convert.ToInt32(Console.ReadLine());
-
-            System.Console.WriteLine("Enter Email-Id: ");
-            contacts.Email = Console.ReadLine();
+           AddressBookMain addrMain = new AddressBookMain();
+           addrMain.AddContact();
             
         }
     }
