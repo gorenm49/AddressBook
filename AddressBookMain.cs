0adb28b9025aa62f7a4f44ff68cce9eab24032f6
Nitya <gorenityanand96@gmail.com>
2022-02-03 17:40:35 +0530

    [Nityanand]:Edit Contact Added

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -36,5 +36,59 @@ namespace AddressBook
 
             addrBook.Add(contacts);
         }
+
+        public void EditContact(string FirstName)
+        {
+            Contacts c1 = new Contacts();
+
+            foreach (var data in addrBook)
+            {
+                if (data.FirstName == FirstName)
+                {
+                    c1 = data;
+                    return;
+                }
+            }
+            System.Console.WriteLine("choose option:\n 1. Last Name\n 2. Address\n 3. City\n 4. State\n 5. Zip\n 6. Phone number\n 7. Email\n 8.Exit");
+            bool flag = true;
+
+            while (flag)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                switch (num)
+                {
+                    case 1:
+                        c1.LastName = Console.ReadLine();
+                        break;
+                    case 2:
+                        c1.Address = Console.ReadLine();
+                        break;
+                    case 3:
+                        c1.City = Console.ReadLine();
+                        break;
+                    case 4:
+                        c1.State = Console.ReadLine();
+                        break;
+                    case 5:
+                        c1.Zip = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 6:
+                        c1.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        c1.Email = Console.ReadLine();
+                        break;
+                    case 8:
+                        flag = false;
+                        break;
+                    default:
+                        System.Console.WriteLine("Choose right option..");
+                        break;
+                }
+                
+            }
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,9 +8,25 @@ namespace AddressBook
         {
             System.Console.WriteLine("Welcome to Address Book Program!!!");
 
-           AddressBookMain addrMain = new AddressBookMain();
-           addrMain.AddContact();
-            
+            System.Console.WriteLine("Select option: \n 1. Add Contact\n 2.Edit Contact\n 3.Exit");
+
+            AddressBookMain addrMain = new AddressBookMain();
+            int num1 = int.Parse(Console.ReadLine());
+
+            switch (num1)
+            {
+                case 1:
+                    addrMain.AddContact();
+                    break;
+                case 2:
+                    addrMain.EditContact("Shree");
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.Console.WriteLine("invalid input");
+                    break;
+            }          
         }
     }
 }
