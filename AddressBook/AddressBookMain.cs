344c08c384325819760bab42494a3247a9f90e1a
gorenm49 <gorenityanand96@gmail.com>
2022-03-19 15:54:19 +0530

    [Nityanand]: Sort Contact By City State Or Zip logic added

---
+++ AddressBook/AddressBookMain.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace AddressBook
+{
+    public class AddressBookMain
+    {
+        public List<Contacts> contact = new List<Contacts>();
+        Contacts addressBook = new Contacts();
+
+        int counter = 0;
+        public void AddContact()
+        {
+            Console.WriteLine("Enter the count of the contact you want to create:");
+            int contactCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= contactCount; i++)
+            {
+                Console.WriteLine("\nEnter the details for " + i + " contact");
+
+                Console.WriteLine("Enter First Name");
+                addressBook.FirstName = Console.ReadLine();
+
+                Console.WriteLine("Enter Last Name");
+                addressBook.LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Address");
+                addressBook.Address = Console.ReadLine();
+
+                Console.WriteLine("Enter a City");
+                addressBook.City = Console.ReadLine();
+
+                Console.WriteLine("Enter a State");
+                addressBook.State = Console.ReadLine();
+
+                Console.WriteLine("Enter a Zip");
+                addressBook.Zip = Console.ReadLine();
+
+                Console.WriteLine("Enter a PhoneNumber");
+                addressBook.Phonenumber = Console.ReadLine();
+
+                Console.WriteLine("Enter an Email");
+                addressBook.Email = Console.ReadLine();
+
+                var checkDuplicate = contact.GroupBy(x => x.FirstName, StringComparer.OrdinalIgnoreCase)
+                         .Where(y => y.Count() > 1)
+                         .Select(z => z.Key)
+                         .ToList();
+                if (checkDuplicate.Equals(addressBook.FirstName))
+                {
+                    Console.WriteLine("Record is already present in the addrebook");
+
+                }
+                else
+                {
+                    contact.Add(addressBook);
+                    counter++;
+                    Display(addressBook);
+                }
+            }
+        }
+
+        public void DeleteContact()
+        {
+            Console.WriteLine("Enter First Name to delete the record");
+            string fname = Console.ReadLine();
+
+            foreach (Contacts item in contact)
+            {
+                if (item.FirstName == fname)
+                {
+                    contact.Remove(item);
+                    Console.WriteLine("Record Deleted Sucessfully");
+                }
+                else
+                {
+                    Console.WriteLine("Record not found");
+                }
+            }
+
+        }
+        public void Display(Contacts contact)
+        {
+            Console.WriteLine("Contact Details " +
+                "\nFirst Name - " + contact.FirstName +
+                "\nLast Name - " + contact.LastName +
+                "\nAddress - " + contact.Address +
+                "\nCity - " + contact.City +
+                "\nState - " + contact.State +
+                "\nZip - " + contact.Zip +
+                "\nPhone Number - " + contact.Phonenumber +
+                "\nEmail Id - " + contact.Email);
+
+        }
+
+        public void EditContact()
+        {
+            Console.WriteLine("Enter The First Name");
+            string Fname = Console.ReadLine();
+
+            foreach (var item in contact)
+            {
+                if (item.FirstName == Fname)
+                {
+
+                    Console.WriteLine("\nEnter the details to update contact:" +
+                        "\n1. First Name" +
+                        "\n2. Last Name" +
+                        "\n3. Address" +
+                        "\n4. city" +
+                        "\n5. State" +
+                        "\n6. Zip" +
+                        "\n7. PhoneNumber" +
+                        "\n8. Email");
+
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    String getString;
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter first Name to Update");
+                            getString = Console.ReadLine();
+                            item.FirstName = getString;
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter Last Name to Update");
+                            getString = Console.ReadLine();
+                            item.LastName = getString;
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter Address to Update");
+                            getString = Console.ReadLine();
+                            item.Address = getString;
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter City Name to Update");
+                            getString = Console.ReadLine();
+                            item.City = getString;
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter State Name to Update");
+                            getString = Console.ReadLine();
+                            item.State = getString;
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter Zip code to Update");
+                            getString = Console.ReadLine();
+                            item.Zip = getString;
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter Phone Number to Update");
+                            getString = Console.ReadLine();
+                            item.Phonenumber = getString;
+                            break;
+                        case 8:
+                            Console.WriteLine("Enter E-mail id to Update");
+                            getString = Console.ReadLine();
+                            item.Email = getString;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Choice");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Record not found.");
+                }
+
+                Display(item);
+            }
+        }
+
+        public void SearchPersonInTheCityOrState(string fname)
+        {
+            foreach (Contacts item in contact)
+            {
+                if (contact.Any(x => x.FirstName == fname))
+                {
+                    Console.WriteLine("Person Found!!");
+                }
+                else
+                {
+                    Console.WriteLine("Person not found");
+                }
+            }
+
+        }
+
+        public void ViewPersonInTheCityOrState(string fname)
+        {
+            foreach (Contacts item in contact)
+            {
+                if (contact.Any(x => x.FirstName == fname))
+                {
+                    Console.WriteLine("Person Found!!");
+                }
+                else
+                {
+                    Console.WriteLine("Person not found");
+                }
+            }
+        }
+
+        public void CountOfContactPersons()
+        {
+            Console.WriteLine("Number of contact Persons" + counter);
+        }
+
+        public void SortContactByName()
+        {
+            try
+            {
+                contact = contact.OrderBy(x => x.FirstName).ToList();
+                Display(addressBook);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public void SortContactByCityStateOrZip(string input)
+        {
+            try
+            {
+                if (input != null)
+                {
+                    contact = contact.OrderBy(x => x.City).ToList();
+                    Display(addressBook);
+                }
+                if (input != null)
+                {
+                    contact = contact.OrderBy(x => x.State).ToList();
+                    Display(addressBook);
+                }
+                if (input != null)
+                {
+                    contact = contact.OrderBy(x => x.Zip).ToList();
+                    Display(addressBook);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+
+        //public void ReadWriteContactIntoTextFile()
+        //{
+        //    try
+        //    {
+        //        string filePath = @"C:\Users\Nityanand Gore\source\repos\AddressBook\AddressBook\AddDataFile.txt";
+        //        using (StreamWriter sw = File.AppendText(filePath))
+        //        {
+        //            foreach (Contacts item in contact)
+        //            {
+        //                sw.WriteLine("\nAddress Book Details Added For " + item.FirstName + " Contact");
+        //                sw.WriteLine(item.FirstName +
+        //                    "\t" + item.LastName +
+        //                    "\t" + item.Address +
+        //                    "\t" + item.City +
+        //                    "\t" + item.State +
+        //                    "\t" + item.Zip +
+        //                    "\t" + item.Phonenumber +
+        //                    "\t" + item.Email);
+        //                sw.Close();
+        //            }
+        //        }
+
+        //        using (StreamReader sr = File.OpenText(filePath))
+        //        {
+        //            string s = "";
+        //            while ((s = sr.ReadLine()) != null)
+        //            {
+        //                Console.WriteLine(s);
+        //            }
+        //        }
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //}
+    }
+}
